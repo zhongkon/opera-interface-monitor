8c92cf81b8924fcd5d5f226e8263f3d1f0879f4a
zhongkon <48372790+zhongkon@users.noreply.github.com>
2019-11-10 19:30:18 +0700

    Update opera_interface_monitor.py

--- opera_interface_monitor.py
+++ opera_interface_monitor.py
@@ -1,13 +1,10 @@
 import cx_Oracle
 import requests
-
 def send_line(msg,token):
     url = 'https://notify-api.line.me/api/notify'
     headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
     r = requests.post(url, headers=headers , data = {'message':msg})
-    #print (r.text)
-
-
+    #print (r.text)    
 con = cx_Oracle.connect('opera/opera@192.168.0.157/opera')
 #print(con.version)
 cur = con.cursor()
