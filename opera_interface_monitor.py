a57d027c633e667dd9991eb85fccb0bd2d3b0f25
zhongkon <48372790+zhongkon@users.noreply.github.com>
2020-02-28 14:16:25 +0700

    Update opera_interface_monitor.py

--- opera_interface_monitor.py
+++ opera_interface_monitor.py
@@ -1,17 +1,31 @@
 import cx_Oracle
 import requests
+
 def send_line(msg,token):
     url = 'https://notify-api.line.me/api/notify'
     headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
     r = requests.post(url, headers=headers , data = {'message':msg})
-    #print (r.text)    
-con = cx_Oracle.connect('opera/opera@192.168.0.157/opera')
-#print(con.version)
-cur = con.cursor()
-cur.execute('SELECT OPERA.IFC_CTRL.NAME,OPERA.IFC_CTRL.ACTIVE,OPERA.IFC_CTRL.IFC_STATUS FROM OPERA.IFC_CTRL WHERE OPERA.IFC_CTRL.ACTIVE = 1')
-for result in cur:
-    #print(result[2])
-    if result[2] == 'STOPPED':
-        send_line(result[0] +' has been ' + result[2],'your_line_notify_token')
-cur.close()
-con.close()
+    #print (r.text)
+
+try:
+    con = cx_Oracle.connect('opera/opera@192.168.0.157/opera')
+    #print(con.version)
+    cur = con.cursor()
+
+except cx_Oracle.DatabaseError as e:
+    errorObj, = e.args
+    send_line(errorObj.message,'youre token hear')
+
+
+try:
+    #cur = con.cursor()
+    cur.execute('SELECT OPERA.IFC_CTRL.NAME,OPERA.IFC_CTRL.ACTIVE,OPERA.IFC_CTRL.IFC_STATUS FROM OPERA.IFC_CTRL WHERE OPERA.IFC_CTRL.ACTIVE = 1')
+    for result in cur:
+        #print(result[2])
+            if result[2] == 'STOPPED':
+                send_line(result[0] +' has been ' + result[2],'Your token hear')
+    cur.close()
+    con.close()
+except cx_Oracle.DatabaseError as e:
+    errorObj, = e.args
+    send_line(errorObj.message,'Your token hear')
